5ade2739448c9d8d28826104b7c410f2f3ea0ac5
Micah Akpan <mickey4sure2k7@gmail.com>
2018-02-11 18:06:29 +0100

    Separate mock data from project file

--- vagrant/projects/restaurant_app/finalProject.py
+++ vagrant/projects/restaurant_app/finalProject.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template
+from mockData import get_mock_restaurant_data, get_single_mock_restaurant_data,\
+                     get_mock_menu_items_data, get_single_mock_menu_item_data
 app = Flask(__name__)
 
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+restaurant = get_single_mock_restaurant_data()
+restaurants = get_mock_restaurant_data()
 
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-
+items = get_mock_menu_items_data()
+item = get_single_mock_menu_item_data()
 
 def getCourses(items):
     courses = set(item.get('course') for item in items) # we need a unique set of menu item courses
